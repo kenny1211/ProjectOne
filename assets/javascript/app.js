7aaaf39177c8fc5e18f4b19e90a472b27b9ec63b
JA <joshappeldev@gmail.com>
2018-09-28 10:53:12 -0400

    firebase and script stuff

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBDZazEgwwzBBqhyPQsvfoKXDjzsSSbZSw",
+    authDomain: "budgetary-app.firebaseapp.com",
+    databaseURL: "https://budgetary-app.firebaseio.com",
+    projectId: "budgetary-app",
+    storageBucket: "budgetary-app.appspot.com",
+    messagingSenderId: "165118016154"
+  };
+  firebase.initializeApp(config);
